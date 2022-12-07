8abe8b726f64f65f05c8de301b9ff07faf4d7429
Shamming <40845877+wsm1685087768@users.noreply.github.com>
2022-12-07 20:14:22 +0800

    优化了中间代码生成时的赋值操作，减少了生成的四元式数量，在四元式申请的新空间名前加入$进行标识

--- intermediate.h
+++ intermediate.h
@@ -18,7 +18,7 @@ class Space { //专门用来分配空间的类
 public:
 	static int cnt;
 	static string allocSpace() {
-		return "t" + std::to_string(++cnt);
+		return "$t" + std::to_string(++cnt);
 	}
 };
 
@@ -100,8 +100,8 @@ public:
 			nowSymbolNode->spaceName = nextSpaceName(1); //直接设置左边E的空间名为产生式右边的空间名
 			break;
 		case 13: //E->id
-			nowSymbolNode->spaceName = Space::allocSpace(); //为左边的E分配空间
-			codes.push_back(Quaternion("=", nextSpaceName(0), nowSymbolNode->spaceName));
+			nowSymbolNode->spaceName = nextSpaceName(0); //直接设置为右边的表达式的空间名
+			//codes.push_back(Quaternion("=", nextSpaceName(0), nowSymbolNode->spaceName));
 			break;
 		case 14: //B->B or B
 			solve(nowSymbolNode->nexts[0]);
